0795e95116bfa11871f55f0a6ab89e6d260372cd
Rob Lorimer <rob.lorimer@gmail.com>
2023-10-08 12:44:46 +1300

    Refactored Computer

--- 2016/12 Leonardos Monorail/aoc_201612.py
+++ 2016/12 Leonardos Monorail/aoc_201612.py
@@ -4,67 +4,65 @@ from dataclasses import dataclass, field
 
 @dataclass
 class Instruction:
-    opcode: str
-    operands: list[str] = field(default_factory=list)
+    op: str  # Opcode
+    ops: list[str] = field(default_factory=list)  # Operands
 
 
 @dataclass
 class Computer:
-    counter: int = 0
-    registers: dict[str, int] = field(default_factory=dict)
-    program: list[Instruction] = field(default_factory=list)
+    pc: int = 0
+    regs: dict[str, int] = field(default_factory=dict)
+    prog: list[Instruction] = field(default_factory=list)
 
     def __post_init__(self):
-        self.registers = {"a": 0, "b": 0, "c": 0, "d": 0}
+        self.regs = {"a": 0, "b": 0, "c": 0, "d": 0}
 
-    def load_program(self, assembly_code: str):
-        for line in assembly_code.splitlines():
+    def load_prog_from_assembly(self, code: str):
+        for line in code.splitlines():
             tokens = line.split(" ")
-            operand_2 = tokens[2] if len(tokens) == 3 else ""
-            self.program.append(
-                Instruction(opcode=tokens[0], operands=[tokens[1], operand_2])
-            )
+            op_2 = tokens[2] if len(tokens) == 3 else ""
+            self.prog.append(Instruction(op=tokens[0], ops=[tokens[1], op_2]))
 
-    def execute_instruction(self):
-        instruction: Instruction = self.program[self.counter]
-        if instruction.opcode == "cpy":
-            self.registers[instruction.operands[1]] = (
-                int(self.registers[instruction.operands[0]])
-                if instruction.operands[0] in self.registers
-                else int(instruction.operands[0])
-            )
-            self.counter += 1
-        elif instruction.opcode == "inc":
-            self.registers[instruction.operands[0]] += 1
-            self.counter += 1
-        elif instruction.opcode == "dec":
-            self.registers[instruction.operands[0]] -= 1
-            self.counter += 1
-        elif instruction.opcode == "jnz":
-            self.counter += (
-                int(instruction.operands[1])
-                if (
-                    int(self.registers[instruction.operands[0]])
-                    if instruction.operands[0] in self.registers
-                    else int(instruction.operands[0])
-                )
-                else 1
-            )
+    def run(self):
+        while 0 <= self.pc < len(self.prog):
+            self.execute_instruction(self.prog[self.pc])
+
+    def execute_instruction(self, instruction: Instruction):
+        do_inst = getattr(self, instruction.op, None)
+        if do_inst:
+            do_inst(instruction.ops)
         else:
             raise ValueError("invalid opcode")
 
-    def run(self):
-        while 0 <= self.counter < len(self.program):
-            self.execute_instruction()
+        if instruction.op in ["cpy", "inc", "dec"]:
+            self.pc += 1
+
+    def cpy(self, ops: list[str]):
+        self.regs[ops[1]] = (
+            int(self.regs[ops[0]]) if ops[0] in self.regs else int(ops[0])
+        )
+
+    def dec(self, ops: list[str]):
+        self.regs[ops[0]] -= 1
+
+    def inc(self, ops: list[str]):
+        self.regs[ops[0]] += 1
+
+    def jnz(self, ops: list[str]):
+        self.pc += (
+            int(ops[1])
+            if (int(self.regs[ops[0]]) if ops[0] in self.regs else int(ops[0]))
+            else 1
+        )
 
 
 @aoc_util.timeit
 def solve(puzzle_input: str, is_b=False) -> str:
     computer = Computer()
-    computer.registers["c"] = 1 if is_b else 0
-    computer.load_program(assembly_code=puzzle_input)
+    computer.regs["c"] = 1 if is_b else 0
+    computer.load_prog_from_assembly(code=puzzle_input)
     computer.run()
-    return str(computer.registers["a"])
+    return str(computer.regs["a"])
 
 
 if __name__ == "__main__":
--- templates/aoc_2016dd_utilities.py
+++ templates/aoc_2016dd_utilities.py
@@ -14,6 +14,60 @@ OPERATORS = {"+": add, "-": sub}
 
 
 @dataclass
+class Instruction:
+    op: str  # Opcode
+    ops: list[str] = field(default_factory=list)  # Operands
+
+
+@dataclass
+class Computer:
+    pc: int = 0
+    regs: dict[str, int] = field(default_factory=dict)
+    prog: list[Instruction] = field(default_factory=list)
+
+    def __post_init__(self):
+        self.regs = {"a": 0, "b": 0, "c": 0, "d": 0}
+
+    def load_prog_from_assembly(self, code: str):
+        for line in code.splitlines():
+            tokens = line.split(" ")
+            op_2 = tokens[2] if len(tokens) == 3 else ""
+            self.prog.append(Instruction(op=tokens[0], ops=[tokens[1], op_2]))
+
+    def run(self):
+        while 0 <= self.pc < len(self.prog):
+            self.execute_instruction(self.prog[self.pc])
+
+    def execute_instruction(self, instruction: Instruction):
+        do_inst = getattr(self, instruction.op, None)
+        if do_inst:
+            do_inst(instruction.ops)
+        else:
+            raise ValueError("invalid opcode")
+
+        if instruction.op in ["cpy", "inc", "dec"]:
+            self.pc += 1
+
+    def cpy(self, ops: list[str]):
+        self.regs[ops[1]] = (
+            int(self.regs[ops[0]]) if ops[0] in self.regs else int(ops[0])
+        )
+
+    def dec(self, ops: list[str]):
+        self.regs[ops[0]] -= 1
+
+    def inc(self, ops: list[str]):
+        self.regs[ops[0]] += 1
+
+    def jnz(self, ops: list[str]):
+        self.pc += (
+            int(ops[1])
+            if (int(self.regs[ops[0]]) if ops[0] in self.regs else int(ops[0]))
+            else 1
+        )
+
+
+@dataclass
 class Movement2D:
     dir: complex = -1j  # North / Up
     position: complex = 0 + 0j
