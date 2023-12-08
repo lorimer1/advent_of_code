245182af2d61deb179eefb69175d531d61f57a51
Rob Lorimer <rob.lorimer@gmail.com>
2023-12-09 12:20:32 +1300

    complete

---
+++ 2023/03 Gear Ratios/aoc_202303_test.py
@@ -0,0 +1,25 @@
+import pytest
+import aoc_202303 as aoc
+
+
+@pytest.mark.parametrize(
+    "puzzle_input,expected,extra",
+    [
+        (
+            "467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598..",
+            "4361",
+            None,
+        )
+    ],
+)
+def test_part_a(puzzle_input, expected, extra):
+    assert aoc.solve(puzzle_input) == expected
+    # assert aoc.solve(puzzle_input, extra) == expected
+
+
+@pytest.mark.parametrize(
+    "puzzle_input,expected,extra", [("", "", ""), ("", "", ""), ("", "", "")]
+)
+def test_part_b(puzzle_input, expected, extra):
+    assert aoc.solve(puzzle_input, is_b=True) == expected
+    # assert aoc.solve_b(puzzle_input, extra, is_b=True) == expected
